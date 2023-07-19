806c8ba544bd4ca18297e92fac0470298bf427fb
seungjong <tmdwhd619@naver.com>
2023-07-19 15:37:07 +0900

    [Feat] concate the 5 image result

--- example/custom_diffusion/inference_single.py
+++ example/custom_diffusion/inference_single.py
@@ -3,8 +3,11 @@ import diffusers
 from diffusers import DiffusionPipeline
 from utils import get_indicator
 import os
-
-
+import numpy as np 
+import cv2
+from einops import rearrange
+from torchvision.utils import make_grid
+from PIL import Image
 
 
 file = open("prompt.txt", "r")
@@ -20,6 +23,8 @@ stable_version = '1-4'
 scheduler = "EulerAncestralDiscreteScheduler"
 use_custom_data = True
 safety = True # prevent to NSFW
+gen_num = 5
+gen_resolution = 128
 #######################################################
 
 
@@ -68,12 +73,30 @@ else:
     
 
 #genrater
-image = pipe(
-    prompt,
-    num_inference_steps=50, #generally use 50
-    guidance_scale=6.0, #text 반영율 6~7
-    eta=1.0, 
-).images[0]
-print(prompt)
-image.save(os.path.join(output_path,prompt+".png")) 
+lst = []
+for count in range(gen_num):
+    image = pipe(
+        prompt,
+        num_inference_steps=50, #generally use 50
+        guidance_scale=6.0, #text 반영율 6~7
+        eta=1.0, 
+    ).images[0]
+    print("Complete to generate [%d / %d] images" % (count+1,gen_num))
+    image.save(os.path.join(output_path,"del"+".png")) 
+    lst.append(image.resize((gen_resolution,gen_resolution)))
+    #(lst[0]).save(os.path.join(output_path,"aa"+".png")) 
+ # creates a new empty image, RGB mode, and size 444 by 95
+
+new_im = Image.new('RGB', (gen_resolution*gen_num,gen_resolution)) #concat palette
+idx = 0
+for elem in lst:
+    new_im.paste(elem, (idx,0))
+    idx += gen_resolution
+new_im.save('test.jpg')   
+
+#concat_img = cv2.hconcat([lst[0],lst[1],lst[2],lst[3],lst[4]])
+#cv2.imwrite(os.path.join(output_path,prompt+".png", concat_img))
+
+#image.save(os.path.join(output_path,prompt+".png")) 
+#concat_image.save(os.path.join(output_path,prompt+".png")) 
 file.close()
